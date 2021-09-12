331862962687ceb3e027b4965e18f70e5148de0e
r3850355 <r3850355@gmail.com>
2021-09-13 01:31:25 +0800

    fix: Day11

--- app/Http/Livewire/Example/Day11Card.php
+++ app/Http/Livewire/Example/Day11Card.php
@@ -6,14 +6,8 @@ use Livewire\Component;
 
 class Day11Card extends Component
 {
-    public $article_json;
     public $article;
 
-    public function mount()
-    {
-        $this->article = json_decode($this->article_json, true);
-    }
-    
     public function render()
     {
         return view('livewire.example.day11-card');
--- resources/views/livewire/example/day11.blade.php
+++ resources/views/livewire/example/day11.blade.php
@@ -2,7 +2,7 @@
     <h2>Day11: 嵌套元件（二）</h2>
 
     @foreach($articles as $article)
-        @livewire('example.day11-card', ['article_json' => json_encode($article)], key('article-2021-' . $article['id']))
+        @livewire('example.day11-card', ['article' => $article], key('article-2021-' . $article['id']))
     @endforeach
 
     <div class="my-5">
